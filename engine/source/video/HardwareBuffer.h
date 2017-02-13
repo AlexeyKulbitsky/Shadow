5f473d08f6447c7d2099c4ce6abd5ad84b277ad6
Alexey Kulbitsky <alex.kulbitsky@gmail.com>
2017-02-13 19:45:30 +0300

    + basic hardwarebuffer for all buffers

---
+++ engine/source/video/HardwareBuffer.h
@@ -0,0 +1,38 @@
+#ifndef SHADOW_HARDWARE_BUFFER_INCLUDE
+#define SHADOW_HARDWARE_BUFFER_INCLUDE
+
+#include "../Globals.h"
+
+namespace sh
+{
+	namespace video
+	{
+		class HardwareBuffer
+		{
+		public:
+			enum Usage
+			{
+				STATIC = 0,
+				DYNAMIC
+			};
+			/*explicit*/ HardwareBuffer(Usage usage) : m_size(0U), m_usage(usage){}
+			virtual ~HardwareBuffer(){}
+
+			virtual void SetData(size_t offset, size_t length, const void* data) = 0;
+			virtual void GetData(size_t offset, size_t length, const void* data) = 0;
+
+			// Returns the size of the buffer in bytes
+			size_t GetSize() const { return m_size; }
+			Usage GetUsage() const { return m_usage; }
+		protected:
+			size_t m_size;
+			Usage m_usage;
+
+		private:
+			//HardwareBuffer() : m_usage(Usage::STATIC){}
+			//HardwareBuffer(const HardwareBuffer& other){}
+		};
+	}
+}
+
+#endif
